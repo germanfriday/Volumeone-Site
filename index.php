b5aa437e17c80850d6cecb0dce4f1e3c6ffcce13
germanfriday <chris@thegermanfriday.com>
2021-06-14 17:50:27 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,533 @@
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<!DOCTYPE php PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xphp1/DTD/xphp1-transitional.dtd">
+<php xmlns="http://www.w3.org/1999/xphp"> <!-- tag closed in footer.php -->
+	
+<head>
+<title>Volumeone | Seasons</title>
+
+<meta http-equiv="content-type" content="text/php; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	
+<script src="../js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="../js/v1.controller.js" type="text/javascript"></script>
+
+<script src="../js/ac_quicktime.js" language="JavaScript" type="text/javascript"></script>
+<script src="../js/qtp_library.js" language="JavaScript" type="text/javascript"></script>
+<link href="../css/qtp_library.css" rel="StyleSheet" type="text/css" />
+
+</head>
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<!-- * * * * * * * * NEW CODE * * * * * * * * -->
+<body id="" class="category"> <!-- tag closed in footer.php -->
+<!-- * * * * * * End: NEW CODE * * * * * * * * -->
+
+<div class="page_wrap">
+
+<div class="header_wrap">
+<div id="header">
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- logo -->
+	
+	<div class="v1_logo">
+		<h2>			<a href="../index.html">
+				<span>VolumeOne</span>
+			</a>
+		</h2>	</div>
+	
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- navigation -->
+	<div class="top_nav">
+		<ul>
+			<!-- * * * * * * * * NEW CODE * * * * * * * * -->
+			<li class="nav_Type">
+				<a class="" href="../type/index.html"><span>Type</span></a>
+			</li>
+			<li class="nav_Motion">
+				<a class="" href="../motion/index.html"><span>Motion</span></a>
+			</li>
+			<li class="nav_Graphics">
+				<a class="" href="../graphics/index.html"><span>Graphics</span></a>
+			</li>
+			<li class="nav_Web">
+				<a class="" href="../web/index.html"><span>Web</span></a>
+			</li>
+			<li class="nav_AboutContact">
+				<a class="" href="../about.php"><span>About + Contact</span></a>
+			</li><!--
+			<li class="nav_Blog">
+				<a href=""><span>Blog</span></a>
+			</li>
+			-->
+			<!-- * * * * * * End: NEW CODE * * * * * * * * -->
+		</ul>
+	</div>
+	
+</div> <!-- close div#header -->
+</div> <!-- close div.header_wrap -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<div class="content_wrap">
+<div id="content">
+
+	<div class="category_projects">
+		
+		<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+		<!-- row 1 -->
+		<div class="project_row">
+			
+			<div class="project first">
+				<a class="thumb" href="30/index.html">
+					<img class="thumb" src="thumbs/thumb_season30.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="30/index.html">The Road Less Traveled</a>
+				</h3>
+				<p class="caption">Short Form Video</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="29/index.html">
+					<img class="thumb" src="thumbs/thumb_season29.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="29/index.html">Meridians</a>
+				</h3>
+				<p class="caption">Short Form Video</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="28/index.html">
+					<img class="thumb" src="thumbs/thumb_season28.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="28/index.html">Suspension of Disbelief</a>
+				</h3>
+				<p class="caption">Video and Flash</p>
+			</div>
+			
+		</div> <!-- end: row 1 -->
+
+		<!-- row 1 -->
+		<div class="project_row">
+			
+			<div class="project first">
+				<a class="thumb" href="27/index.html">
+					<img class="thumb" src="thumbs/thumb_season27.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="27/index.html">Spaceship Earth</a>
+				</h3>
+				<p class="caption">Video and Flash</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="26/index.html">
+					<img class="thumb" src="thumbs/thumb_season26.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="26/index.html">Mean Streak</a>
+				</h3>
+				<p class="caption">Video and Flash</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="25/index.html">
+					<img class="thumb" src="thumbs/thumb_season25.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="25/index.html">Brutal Honesty</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+		</div> <!-- end: row 1 -->
+
+		<!-- row 1 -->
+		<div class="project_row">
+			
+			<div class="project first">
+				<a class="thumb" href="24/index.html">
+					<img class="thumb" src="thumbs/thumb_season24.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="24/index.html">Blazing Wheels</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="23/index.html">
+					<img class="thumb" src="thumbs/thumb_season23.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="23/index.html">Apologue</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="22/index.html">
+					<img class="thumb" src="thumbs/thumb_season22.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="22/index.html">The Long Haul</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+		</div> <!-- end: row 1 -->		
+
+	
+		<!-- row 1 -->
+		<div class="project_row">
+			
+			<div class="project first">
+				<a class="thumb" href="21/index.html">
+					<img class="thumb" src="thumbs/thumb_season21.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="21/index.html">Eagle Eyes</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+
+			<div class="project">
+				<a class="thumb" href="20/index.html">
+					<img class="thumb" src="thumbs/thumb_season20.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="20/index.html">Good Neighbor Policy</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>			
+			
+			<div class="project">
+				<a class="thumb" href="19/index.html">
+					<img class="thumb" src="thumbs/thumb_season19.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="19/index.html">Junior Rangers</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+		
+			
+		</div> <!-- end: row 1 -->	
+
+		<!-- row 1 -->
+		<div class="project_row">
+
+			<div class="project first">
+				<a class="thumb" href="18/index.html">
+					<img class="thumb" src="thumbs/thumb_season18.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="18/index.html">Breaking the Rulez</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+						
+			<div class="project first">
+				<a class="thumb" href="17/index.html">
+					<img class="thumb" src="thumbs/thumb_season17.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="17/index.html">Spring Hygiene</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+			
+			<div class="project">
+				<a class="thumb" href="16/index.html">
+					<img class="thumb" src="thumbs/thumb_season16.jpg">
+				</a>
+				
+				<h3 class="thumb_title">
+					<a href="16/index.html">Consumed / Perceived</a>
+				</h3>
+				<p class="caption">Flash Experiment</p>
+			</div>
+			
+					
+		</div> <!-- end: row 1 -->		
+
+		<!-- row 1 -->
+		<div class="project_row">
+			
+		<div class="project first">
+			<a class="thumb" href="15/index.html">
+				<img class="thumb" src="thumbs/thumb_season15.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="15/index.html">Autumn Absentee</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+	
+		<div class="project">
+			<a class="thumb" href="14/index.html">
+				<img class="thumb" src="thumbs/thumb_season14.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="14/index.html">Young Achiever</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+		
+		
+		<div class="project">
+			<a class="thumb" href="13/index.html">
+				<img class="thumb" src="thumbs/thumb_season13.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="13/index.html">Familiar Paradise</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>	
+		</div> <!-- end: row 1 -->	
+		
+		<!-- row 1 -->
+		<div class="project_row">
+			
+		<div class="project first">
+			<a class="thumb" href="12/index.html">
+				<img class="thumb" src="thumbs/thumb_season12.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="12/index.html">The Icescape</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+	
+		<div class="project">
+			<a class="thumb" href="11/index.html">
+				<img class="thumb" src="thumbs/thumb_season11.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="11/index.html">Transient Notations</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+		
+		
+		<div class="project">
+			<a class="thumb" href="10/index.html">
+				<img class="thumb" src="thumbs/thumb_season10.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="10/index.html">The Deep End</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>	
+		</div> <!-- end: row 1 -->	
+		
+		<!-- row 1 -->
+		<div class="project_row">
+			
+		<div class="project first">
+			<a class="thumb" href="9/index.html">
+				<img class="thumb" src="thumbs/thumb_season9.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="9/index.html">Assembly Lines</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+	
+		<div class="project">
+			<a class="thumb" href="8/index.html">
+				<img class="thumb" src="thumbs/thumb_season8.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="8/index.html">Permanent Press</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>
+		
+		
+		<div class="project">
+			<a class="thumb" href="7/index.html">
+				<img class="thumb" src="thumbs/thumb_season7.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="7/index.html">Present Nostalgia</a>
+			</h3>
+			<p class="caption">Flash Experiment</p>
+		</div>	
+		</div> <!-- end: row 1 -->	
+		
+		<!-- row 1 -->
+		<div class="project_row">
+			
+		<div class="project first">
+			<a class="thumb" href="6/index.html">
+				<img class="thumb" src="thumbs/thumb_season6.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="6/index.html">The Tarmac</a>
+			</h3>
+			<p class="caption">Flash/HTML Experiment</p>
+		</div>
+	
+		<div class="project">
+			<a class="thumb" href="5/index.html">
+				<img class="thumb" src="thumbs/thumb_season5.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="5/index.html">The Blinders</a>
+			</h3>
+			<p class="caption">Flash/HTML Experiment</p>
+		</div>
+		
+		
+		<div class="project">
+			<a class="thumb" href="4/index.html">
+				<img class="thumb" src="thumbs/thumb_season4.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="4/index.html">Ave of the Americas</a>
+			</h3>
+			<p class="caption">Flash/HTML Experiment</p>
+		</div>	
+		</div> <!-- end: row 1 -->	
+		
+		<!-- row 1 -->
+		<div class="project_row">
+			
+		<div class="project first">
+			<a class="thumb" href="3/index.html">
+				<img class="thumb" src="thumbs/thumb_season3.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="3/index.html">The Great Society</a>
+			</h3>
+			<p class="caption">HTML Experiment</p>
+		</div>
+	
+		<div class="project">
+			<a class="thumb" href="2/index.html">
+				<img class="thumb" src="thumbs/thumb_season2.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="2/index.html">Summer Invitational</a>
+			</h3>
+			<p class="caption">HTML Experiment</p>
+		</div>
+		
+		
+		<div class="project">
+			<a class="thumb" href="1/index.html">
+				<img class="thumb" src="thumbs/thumb_season1.jpg">
+			</a>
+			
+			<h3 class="thumb_title">
+				<a href="1/index.html">Science of Sound</a>
+			</h3>
+			<p class="caption">HTML Experiment</p>
+		</div>	
+		</div> <!-- end: row 1 -->
+					
+	</div>
+	
+</div> <!-- close div#content -->
+</div> <!-- close div.content_wrap -->
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<div class="footer_wrap">
+<div id="footer">
+	
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- navigation -->
+	<div class="bottom_nav">
+		<ul>
+			<li class="nav_Type">
+				<a href="../type/index.html">Type</a>
+			</li>
+			<span>/</span>
+			<li class="nav_Graphics">
+				<a href="../graphics/index.html">Graphics</a>
+			</li>			
+			<span>/</span>
+			<li class="nav_Motion">
+				<a href="../motion/index.html">Motion</a>
+			</li>
+			<span>/</span>
+			<li class="nav_Web">
+				<a href="../web/index.html">Web</a>
+			</li>
+			<span>/</span>
+			<li class="nav_AboutContact">
+				<a href="../about.php">About + Contact</a>
+			</li><!--
+			<span>/</span>
+			<li class="nav_Blog">
+				<a href="">Blog</a>
+			</li>-->
+		</ul>
+	</div>
+	<div class="bottom_nav_copyright">
+		<ul>
+			<li class="nav_copyright">
+			&copy; 2011 Volumeone LLC. All Right Reserved.
+			</li>
+		</ul>	
+	</div?>
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"> 
+	</script> 
+	<script type="text/javascript"> 
+	_uacct = "UA-553156-1";
+	urchinTracker();
+	</script>
+</div> <!-- close div#footer -->
+</div> <!-- close div.footer_wrap -->
+
+</div> <!-- close div.page_wrap -->
+</body> <!-- tag opened in header.php -->
+</php> <!-- tag opened in header.php -->
+<!-- Localized -->
