445b71e22c0119f6c4144112416c2bb87dce9523
germanfriday <chris@thegermanfriday.com>
2021-06-14 17:27:07 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,185 @@
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<!DOCTYPE php PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xphp1/DTD/xphp1-transitional.dtd">
+<php xmlns="http://www.w3.org/1999/xphp"> <!-- tag closed in footer.php -->
+	
+<head>
+<title></title>
+
+<meta http-equiv="content-type" content="text/php; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+	
+<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="js/v1.controller.js" type="text/javascript"></script>
+
+<script src="js/ac_quicktime.js" language="JavaScript" type="text/javascript"></script>
+<script src="js/qtp_library.js" language="JavaScript" type="text/javascript"></script>
+<link href="css/qtp_library.css" rel="StyleSheet" type="text/css" />
+
+</head>
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<!-- * * * * * * * * NEW CODE * * * * * * * * -->
+<body id="about_contact" class="about_contact"> <!-- tag closed in footer.php -->
+<!-- * * * * * * End: NEW CODE * * * * * * * * -->
+
+<div class="page_wrap">
+
+<div class="header_wrap">
+<div id="header">
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- logo -->
+	
+	<div class="v1_logo">
+		<h2>			<a href="index.html">
+				<span>VolumeOne</span>
+			</a>
+		</h2>	</div>
+	
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- navigation -->
+	<div class="top_nav">
+		<ul>
+			<!-- * * * * * * * * NEW CODE * * * * * * * * -->
+			<li class="nav_Type">
+				<a class="" href="type/index.html"><span>Type</span></a>
+			</li>
+			<li class="nav_Motion">
+				<a class="" href="motion/index.html"><span>Motion</span></a>
+			</li>
+			<li class="nav_Graphics">
+				<a class="" href="graphics/index.html"><span>Graphics</span></a>
+			</li>
+			<li class="nav_Web">
+				<a class="" href="web/index.html"><span>Web</span></a>
+			</li>
+			<li class="nav_AboutContact">
+				<a class="active" href="about.php"><span>About + Contact</span></a>
+			</li><!--
+			<li class="nav_Blog">
+				<a href=""><span>Blog</span></a>
+			</li>
+			-->
+			<!-- * * * * * * End: NEW CODE * * * * * * * * -->
+		</ul>
+	</div>
+	
+</div> <!-- close div#header -->
+</div> <!-- close div.header_wrap -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<div class="content_wrap">
+<div id="content">
+
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	<!-- Contact (left column) -->
+	<div class="contact left_col">
+		<h2><span>Contact Us</span></h2>
+		
+		<div class="v1_info">
+			<h3>Volumeone Studios</h3>
+			
+			<p class="description">
+				Volumeone LLC<br>
+				190 North 10th Street. #305<br>
+				Brooklyn, NY 11211<br>
+				T. 212.851.6074<br>
+				<br>
+				Email: <a href="mailto:nyc@volumeone.com">nyc@volumeone.com</a><br>
+				Twitter: <a href="http://twitter.com/mattovolumeone">@mattovolumeone</a><br>
+				Linked In: <a href="http://www.linkedin.com/profile/view?id=2705366&authType=name&authToken=Ts3u&locale=en_US&pvs=pp&trk=ppro_viewmore">Matt Owens</a>
+			</p>
+		</div>
+		
+	</div> <!-- end: div.contact -->
+	
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	<!-- About (right column) -->
+	<div class="about right_col">
+		<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+		<div class="top_image">
+			<img src="images/about-top_image.jpg">
+		</div>
+		
+		<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+		<div class="about_copy">
+			<h1><span>About Volumeone</span></h1>
+			
+			<p class="first">Volumeone is a design studio that brings a multi-disciplinary approach to the creation of visual solutions for print, motion and digital media. Founded by Cranbrook Design graduate Matt Owens, Volumeone strives to synthesize visual exploration and smart design solutions while maintaining the highest level of design expertise and freshness of approach. All design challenges involve a dialogue between client, creative and the needs of the project and Volumeone works to illuminate and open up this dialogue in an effort to develop the best solutions possible.</p>
+			
+			<p>Currently, Matt is a founding partner of <a href="http://athleticsnyc.com/">Athletics</a>, a design collective in New York City. Athletics is comprised of some of today's best and brightest talents in their respective fields. With skill sets ranging from graphic design, illustration, art direction, motion graphics and video directing to web design and development, Athletics members have come together to leverage their talents and energies for a wide range of clients and projects. Each Athletics member brings unique insight and expertise to the collective. From publishing books and records, playing and recording music, producing and directing videos, and running art galleries, Athletics members have a strong commitment to creative thinking and a broad range of experience on the front lines of cultural production.</p>
+			
+			<p>
+Matt has spoken internationally and his work has been recognized by the Art Directors Club, American Center for Design, the AIGA and multiple domestic and foreign design publications.
+			</p>	
+
+			
+		</div>
+		
+	</div>
+	
+</div> <!-- close div#content -->
+</div> <!-- close div.content_wrap -->
+
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+
+<div class="footer_wrap">
+<div id="footer">
+	
+	<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * -->
+	
+	<!-- navigation -->
+	<div class="bottom_nav">
+		<ul>
+			<li class="nav_Type">
+				<a href="type/index.html">Type</a>
+			</li>
+			<span>/</span>
+			<li class="nav_Graphics">
+				<a href="graphics/index.html">Graphics</a>
+			</li>			
+			<span>/</span>
+			<li class="nav_Motion">
+				<a href="motion/index.html">Motion</a>
+			</li>
+			<span>/</span>
+			<li class="nav_Web">
+				<a href="web/index.html">Web</a>
+			</li>
+			<span>/</span>
+			<li class="nav_AboutContact">
+				<a href="about.php">About + Contact</a>
+			</li><!--
+			<span>/</span>
+			<li class="nav_Blog">
+				<a href="">Blog</a>
+			</li>-->
+		</ul>
+	</div>
+	<div class="bottom_nav_copyright">
+		<ul>
+			<li class="nav_copyright">
+			&copy; 2011 Volumeone LLC. All Right Reserved.
+			</li>
+		</ul>	
+	</div?>
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"> 
+	</script> 
+	<script type="text/javascript"> 
+	_uacct = "UA-553156-1";
+	urchinTracker();
+	</script>
+</div> <!-- close div#footer -->
+</div> <!-- close div.footer_wrap -->
+
+</div> <!-- close div.page_wrap -->
+</body> <!-- tag opened in header.php -->
+</php> <!-- tag opened in header.php -->
+<!-- Localized -->
